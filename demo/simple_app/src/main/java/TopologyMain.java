a3acda546589ad62c192bdd30e4b24ab9c1069a6
Luan Cestari <luan.cestari@gmail.com>
2013-07-16 00:09:23 -0300

    kafka

--- demo/simple_app/src/main/java/TopologyMain.java
+++ demo/simple_app/src/main/java/TopologyMain.java
@@ -2,12 +2,36 @@ import backtype.storm.Config;
 import backtype.storm.LocalCluster;
 import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.tuple.Fields;
+import com.google.common.collect.ImmutableList;
+import storm.kafka.SpoutConfig;
+import storm.kafka.KafkaSpout;
+import storm.kafka.KafkaConfig;
+import storm.kafka.KafkaConfig.StaticHosts;
+import storm.kafka.KafkaConfig.ZkHosts;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class TopologyMain {
 	public static void main(String[] args) throws InterruptedException {
 		TopologyBuilder builder = new TopologyBuilder();
-		builder.setSpout("word-reader",new WordReaderSpout());
+		//List<String> l = new ArrayList<String>();
+		//l.add("localhost:");
+		//l.add("localhost:59963");
+		//l.add("localhost:35256");
+		//l.add("localhost:9092");
+		//StaticHosts m = null;
+		//m=StaticHosts.fromHostString(
+		//		l, // list of Kafka brokers
+		//		8 // number of partitions per host
+		//);
+		ZkHosts m = new ZkHosts("localhost:9092","/");
+		SpoutConfig spoutConfig = new SpoutConfig(
+			m,
+			"test", // topic to read from
+			"/kafkastorm", // the root path in Zookeeper for the spout to store the consumer offsets
+			"discovery"); // an id for this consumer for storing the consumer offsets in Zookeeper
+			builder.setSpout("word-reader",new KafkaSpout(spoutConfig));
 		builder.setBolt("word-normalizer", new WordNormalizerBolt())
 			.shuffleGrouping("word-reader");
 		builder.setBolt("word-counter", new WordCounterBolt(),1)
